96ed6fbc5199c0e3a64f9ac83ab8dcba648e4c05
jeganathan I <jegan4044@gmail.com>
2025-06-25 18:54:31 +0000

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,16 +8,6 @@ const adminRoutes = require("./Routes/adminRoutes");
 const userRoutes = require("./Routes/userRoutes");
 const cors = require("cors");
 
-const app = express();require("dotenv").config();
-const express = require("express");
-const path = require("path"); 
-const connectDB = require("./config/db");
-const authRoutes = require("./Routes/authRoutes");
-const sellerRoutes = require("./Routes/sellerRoutes");
-const adminRoutes = require("./Routes/adminRoutes");
-const userRoutes = require("./Routes/userRoutes");
-const cors = require("cors");
-
 const app = express();
 
 app.use(
@@ -41,37 +31,7 @@ app.use(
   "/uploads",
   express.static(path.join(__dirname, "uploads"), {
     setHeaders: (res, path) => {
-        res.set("Access-Control-Allow-Origin", process.env.CLIENT_URL);
-    },
-  })
-);
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-
-app.use(
-    cors({
-        origin: process.env.CLIENT_URL || "*",
-        methods:["GET", "POST", "PUT", "DELETE"],
-        allowedHeaders:["Content-Type", "Authorization"]
-    })
-)
-app.options("*", cors());
-connectDB();
-
-app.use(express.json());
-
-app.use("/api/auth", authRoutes);
-app.use("/api/product", sellerRoutes);
-app.use("/api/admin", adminRoutes);
-app.use("/api/users", userRoutes);
-
-app.use(
-  "/uploads",
-  express.static(path.join(__dirname, "uploads"), {
-    setHeaders: (res, path) => {
-      res.set("Access-Control-Allow-Origin", "http://localhost:5173/");
+      res.set("Access-Control-Allow-Origin", process.env.CLIENT_URL);
     },
   })
 );
