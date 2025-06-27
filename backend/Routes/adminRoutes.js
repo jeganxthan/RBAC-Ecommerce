c51d1386c16669aed8bad65da8684ccc0d0ce5e7
jeganathan I <jegan4044@gmail.com>
2025-06-27 06:11:46 +0000

    Update adminRoutes.js

--- backend/Routes/adminRoutes.js
+++ backend/Routes/adminRoutes.js
@@ -1,39 +1,33 @@
 const express = require('express');
 const {
-    getAllSeller,
-    getSeller,
-    getSellerAllProducts,
-    getSellerproduct,
-    getAllProduct,
-    getProduct,
-    deleteSellerProduct,
-    getDashboardStats,
-    blockUser,
-    unblockUser,
+  getAllSeller,
+  getSeller,
+  getSellerAllProducts,
+  getSellerproduct,
+  getAllProduct,
+  getProduct,
+  deleteSellerProduct,
+  getDashboardStats,
+  blockUser,
+  unblockUser,
 } = require('../controller/adminController'); 
 
 const { protect, adminOnly } = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
-// Sellers
 router.get("/seller", protect, adminOnly, getAllSeller);
 router.get("/seller/:id", protect, adminOnly, getSeller);
 router.put("/seller/block/:id", protect, adminOnly, blockUser);
 router.put("/seller/unblock/:id", protect, adminOnly, unblockUser);
 
-// Seller Products
 router.get("/seller-products", protect, adminOnly, getSellerAllProducts);
 router.get("/seller-products/:sellerId/:productId", protect, adminOnly, getSellerproduct);
 
-
-// Products
 router.get("/products", protect, adminOnly, getAllProduct);
 router.get("/products/:id", protect, adminOnly, getProduct);
 router.delete("/products/:id", protect, adminOnly, deleteSellerProduct);
 
-// Dashboard
 router.get("/", protect, adminOnly, getDashboardStats);
 
-module.exports = router; 
-
+module.exports = router;
