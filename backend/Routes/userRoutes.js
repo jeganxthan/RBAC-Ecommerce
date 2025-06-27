08eb0f7114e8816a7b993bd449ad81dcb5c69987
jeganathan I <jegan4044@gmail.com>
2025-06-27 06:12:02 +0000

    Update userRoutes.js

--- backend/Routes/userRoutes.js
+++ backend/Routes/userRoutes.js
@@ -1,26 +1,29 @@
-const express = require("express")
+const express = require("express");
+const {
+  getAllProduct,
+  getProduct,
+  getUserProfile,
+  updateUserProfile,
+  addToCart,
+  getCart,
+  removeFromCart,
+  getProductSuggestions,
+  getCartCount, 
+  payment
+} = require("../controller/userController");
 const { protect } = require("../middleware/authMiddleware");
-const { getAllProduct,
-    getProduct,
-    getUserProfile,
-    updateUserProfile,
-    addToCart,
-    getCart,
-    removeFromCart,
-    getProductSuggestions,
-    getCartCount, 
-    payment} = require("../controller/userController");
 
 const router = express.Router();
 
-router.get('/', protect, getAllProduct);
-router.get('/profile', protect, getUserProfile);
-router.get('/getcart', protect, getCart);
-router.get('/cartcount', protect, getCartCount);
-router.get('/search', getProductSuggestions);
-router.put('/updateprofile', protect, updateUserProfile);
-router.post('/addcart', protect, addToCart);
-router.post('/payment', protect, payment);
-router.delete('/cart/:productId', protect, removeFromCart);
-router.get('/:id', protect, getProduct);
+router.get("/", protect, getAllProduct);
+router.get("/profile", protect, getUserProfile);
+router.get("/getcart", protect, getCart);
+router.get("/cartcount", protect, getCartCount);
+router.get("/search", getProductSuggestions);
+router.put("/updateprofile", protect, updateUserProfile);
+router.post("/addcart", protect, addToCart);
+router.post("/payment", protect, payment);
+router.delete("/cart/:productId", protect, removeFromCart);
+router.get("/:id", protect, getProduct);
+
 module.exports = router;
