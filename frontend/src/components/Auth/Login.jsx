e99fcf623f30ccbe68e2fae08609e53aa139524e
jeganathan I <jegan4044@gmail.com>
2025-06-28 05:34:07 +0000

    Update Login.jsx

--- frontend/src/components/Auth/Login.jsx
+++ frontend/src/components/Auth/Login.jsx
@@ -53,7 +53,7 @@ const Login = ({ setCurrentPage }) => {
         <Input label="Email Address" placeholder="eg. John Doe" value={email} onChange={({ target }) => setEmail(target.value)} type="text" />
         <Input label="Password" placeholder="Min 8 Characters" value={password} onChange={({ target }) => setPassword(target.value)} type="password" />
         {error && <p className='text-red-500 text-sm'>{error}</p>}
-        <button className='bg-[#1947a8] w-full p-2 hover:bg-[#638ee8] text-white'>Login</button>
+        <button type="submit" className='bg-[#1947a8] w-full p-2 hover:bg-[#638ee8] text-white'>Login</button>
         <p>Don't have an account?{" "}
           <button className='text-violet-950 underline' onClick={() => setCurrentPage('signup')}>
             SignUp
