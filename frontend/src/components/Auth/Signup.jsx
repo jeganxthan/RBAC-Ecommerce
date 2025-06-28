ecb84c89cba3fdffafd2392f5b1b42984fda28bb
jeganathan I <jegan4044@gmail.com>
2025-06-28 05:37:23 +0000

    Update Signup.jsx

--- frontend/src/components/Auth/Signup.jsx
+++ frontend/src/components/Auth/Signup.jsx
@@ -97,7 +97,7 @@ const Signup = ({ setCurrentPage }) => {
                 </label>
                 {error && <p className='text-red-500 text-sm'>{error}</p>}
 
-                <button className='bg-[#1947a8] w-full p-2 hover:bg-[#638ee8] text-white' type='submit'>SignUp</button>
+                <button type="submit" className='bg-[#1947a8] w-full p-2 hover:bg-[#638ee8] text-white' type='submit'>SignUp</button>
                 <p>Already have an account{" "}
                     <button className='text-violet-950 underline' onClick={() => setCurrentPage('login')} type='button'>
                         Login
